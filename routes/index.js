f5fce60834b5d7132e5d1ea5da01ab4facdeab37
Torgeir Beyer <torgeirbeyer@IRONHACKs-Mac-mini.local>
2017-11-16 11:02:18 +0100

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const mongoose = require("mongoose");
+
+const Picture = require('../models/pictures');
+
+const upload = multer({ dest: './public/uploads/' });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+// POST file upload
+router
+  .post('/upload', upload.single('photo'), (req, res, next) => {
+    const pic = new Picture({
+      name: req.body.name,
+      pic_path: `/uploads/${req.file.filename}`,
+      pic_name: req.file.originalname
+    });
+
+    pic.save((err) => {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect('/');
+      }
+    });
+  });
 
 module.exports = router;
